2e3a23734182473b1fa5d683352bb220df5220f3
Sam Rankin <sam.rankin@me.com>
2017-09-16 17:29:34 -0400

    implemented engine limits and new output values

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,16 @@ extern crate forkengine;
 extern crate rocket;
 extern crate rocket_contrib;
 
-use forkengine::{Runtime, RuntimeSnapshot};
+use forkengine::{Runtime, RuntimeProduct};
 use rocket_contrib::Json;
 
-fn snapshots_to_json(snapshots: Vec<RuntimeSnapshot>) -> String {
+static RUNTIME_EXECUTION_LIMIT: usize = 500;
+static RUNTIME_MEMORY_LIMIT: usize = 32000;
+
+fn product_to_json(product: RuntimeProduct) -> String {
 	let mut serialized_snapshots = Vec::new();
 
-	for snapshot in snapshots {
+	for snapshot in product.snapshots {
 		serialized_snapshots.push(
 			json!({
 				"memory": snapshot.memory,
@@ -28,7 +31,12 @@ fn snapshots_to_json(snapshots: Vec<RuntimeSnapshot>) -> String {
 		);
 	}
 
-	json!(serialized_snapshots).to_string()
+	json!({
+		"snapshots": serialized_snapshots,
+		"output": product.output,
+		"executions": product.executions,
+		"time": product.time
+	}).to_string()
 }
 
 #[derive(Deserialize)]
@@ -39,10 +47,16 @@ struct InterpretationRequest {
 
 #[post("/request_interpretation", data = "<request>")]
 fn handle_json_request(request: Json<InterpretationRequest>) -> String {
-	let snapshots = Runtime::new(request.instructions.clone(), request.input.as_bytes().to_vec()).run().snapshots;
-	print!("executed {} instructions, ", snapshots.len());
-	let json = snapshots_to_json(snapshots);
+
+	let product = Runtime::with_limits(
+		request.instructions.clone(), request.input.as_bytes().to_vec(),
+		RUNTIME_EXECUTION_LIMIT, RUNTIME_MEMORY_LIMIT
+	).run();
+
+	print!("executed {} instructions in {:.2} ms, ", product.executions, (product.time as f64 / 1000000.0) as f64);
+	let json = product_to_json(product);
 	println!("returned {} bytes", json.len());
+
 	json
 }
 
